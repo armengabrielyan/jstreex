922b8987bc6cfdd78b18be7fd4b3205023daa20a
Armen Gabrielyan <aggabriel88@gmail.com>
2015-03-15 17:48:43 +0100

    Added additional API layer

---
+++ data.js
@@ -0,0 +1,95 @@
+var folders =
+  [
+    {
+      id: 1,
+      title: "Hello World 1",
+      expanded: true,
+      sub: [
+        {
+          id: 11,
+          title: "Hello World 1.1",
+          sub: [
+            {
+              id: 111,
+              title: "Hello World 1.1.1",
+              sub: [
+                {
+                  id: 1111,
+                  title: "Hello World 1.1.1.1",
+                  sub: []
+                },
+                {
+                  id: 1112,
+                  title: "Hello World 1.1.1.2",
+                  sub: [
+                    {
+                      id: 11121,
+                      title: "Hello World 1.1.1.2.1",
+                      sub: [
+                        {
+                          id: 111211,
+                          title: "Hello World 1.1.1.2.1.1",
+                          sub: [
+                            {
+                              id: 1112111,
+                              title: "Hello World 1.1.1.2.1.1.1",
+                              sub: [
+                                {
+                                  id: 11121111,
+                                  title: "Hello World 1.1.1.2.1.1.1.1",
+                                  sub: []
+                                },
+                                {
+                                  id: 11121112,
+                                  title: "Hello World 1.1.1.2.1.1.1.2",
+                                  sub: [
+
+                                  ]
+                                }
+                              ]
+                            }
+                          ]
+                        },
+                        {
+                          id: 111212,
+                          title: "Hello World 1.1.1.2.1.2",
+                          sub: []
+                        },
+                        {
+                          id: 111213,
+                          title: "Hello World 1.1.1.2.1.3",
+                          sub: [
+                            {
+                              id: 1112131,
+                              title: "Hello World 1.1.1.2.1.3.1",
+                              sub: []
+                            }
+                          ]
+                        }
+                      ]
+                    }
+                  ]
+                }
+              ]
+            }
+          ]
+        },
+        {
+          id: 12,
+          title: "Hello World 1.2",
+          sub: []
+        },
+        {
+          id: 13,
+          title: "Hello World 1.3",
+          sub: [
+            {
+              id: 131,
+              title: "Hello World 1.3.1",
+              sub: []
+            }
+          ]
+        }
+      ]
+    }
+  ];
